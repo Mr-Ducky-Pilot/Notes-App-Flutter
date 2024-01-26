1791ca51bed734ac9c27bae5107b32234fff7551
Mr-Ducky-Pilot <mrghostduck@gmail.com>
2024-01-27 02:18:20 +0530

    modified:   lib/main.dart 	renamed:    lib/verifyemail_view.dart -> lib/views/verifyemail_view.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
-import 'package:mynotes/verifyemail_view.dart';
+import 'package:mynotes/views/verifyemail_view.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 
